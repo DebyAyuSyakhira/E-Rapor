1550c5dc4750bf7d4b511598b6b89f1ebd14cf52
tonnyspt <tonysyaputra771@gmail.com>
2024-06-07 21:15:16 +0700

    add splash screen

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,24 +1,33 @@
+import 'package:e_rapor/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/splash_controller.dart';
 
 class SplashView extends GetView<SplashController> {
-  const SplashView({Key? key}) : super(key: key);
+  const SplashView({super.key});
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 5), () {
+      Get.offNamed(Routes.REGISTER);
+    });
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SplashView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'SplashView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Image.asset("images/bg_top.png", fit: BoxFit.contain),
+            ),
+            SizedBox(
+                child: Image.asset("images/logo_splash.png")),
+            SizedBox(
+                width: double.maxFinite,
+                child: Image.asset(
+                  "images/bg.png",
+                  fit: BoxFit.contain,
+                  alignment: Alignment.bottomCenter,
+                ))
+          ]),
     );
   }
 }
